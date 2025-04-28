fff43a3ec787dccd365bd08ba5c66ffed98fb613
Andrey Moura <andreymds88@gmail.com>
2025-04-28 18:08:15 -0300

    wip: Working on UI extension

---
+++ src/builtin_libs/ui_extension.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+
+#include <andy/lang/api.hpp>
+#include <andy/lang/extension.hpp>
+
+#include "ui_extension/app_class.cpp"
+#include "ui_extension/frame_class.cpp"
+#include "ui_extension/dialog_class.cpp"
+
+class ui_extension : public andy::lang::extension
+{
+public:
+    ui_extension()
+        : andy::lang::extension("ui")
+    {
+    }
+protected:
+    std::shared_ptr<andy::lang::object> application_instance;
+public:
+    virtual void load(andy::lang::interpreter* interpreter) override
+    {
+        auto UIClass = std::make_shared<andy::lang::structure>("UI");
+
+        auto AppClass = create_app_class(interpreter);
+        auto FrameClass = create_frame_class(interpreter);
+        auto DialogClass = create_dialog_class(interpreter);
+
+        auto app_obj    = andy::lang::object::create(interpreter, interpreter->ClassClass, AppClass);
+        auto frame_obj  = andy::lang::object::create(interpreter, interpreter->ClassClass, FrameClass);
+        auto dialog_obj = andy::lang::object::create(interpreter, interpreter->ClassClass, DialogClass);
+
+        app_obj->cls->instance_methods["new"].call(app_obj);
+        app_obj->cls->instance_methods["new"].call(frame_obj);
+        app_obj->cls->instance_methods["new"].call(dialog_obj);
+
+        UIClass->class_variables = {
+            { "Application", app_obj    },
+            { "Frame",       frame_obj  },
+            { "Dialog",      dialog_obj },
+        };
+
+        interpreter->load(UIClass);
+    }
+
+    virtual void start(andy::lang::interpreter* interpreter) override
+    {
+        auto UI = interpreter->find_class("UI");
+
+        if(!UI) {
+            throw std::runtime_error("UI namespace not found");
+        }
+
+        auto AppClass = UI->class_variables["Application"]->as<std::shared_ptr<andy::lang::structure>>();
+
+        if(AppClass->deriveds.empty()) {
+            throw std::runtime_error("UI.Application has no derived classes");
+        }
+
+        application_instance = andy::lang::object::instantiate(interpreter, AppClass->deriveds.front(), nullptr);
+    }
+};
+
+std::shared_ptr<andy::lang::extension> create_ui_extension()
+{
+    return std::make_shared<ui_extension>();
+}
---
+++ src/builtin_libs/ui_extension/app_class.cpp
@@ -0,0 +1,54 @@
+#include <andy/lang/api.hpp>
+#include <andy/ui/app.hpp>
+
+class uvalang_ui_app : public andy::ui::app
+{
+public:
+    uvalang_ui_app(andy::lang::interpreter* __interpreter, std::shared_ptr<andy::lang::object> __application_instance)
+        : andy::ui::app("uva", "uva"), interpreter(__interpreter)
+    {
+        application_instance = __application_instance;
+    }
+protected:
+    andy::lang::interpreter* interpreter = nullptr;
+    std::shared_ptr<andy::lang::object> application_instance;
+public:
+    virtual void on_init(int argc, char** argv) override
+    {
+        auto run_it = application_instance->cls->instance_methods.find("run");
+
+        if(run_it == application_instance->cls->instance_methods.end()) {
+            throw std::runtime_error("run method not defined in class Application");
+        }
+
+        andy::lang::function_call run_it_call = {
+            "run",
+            application_instance->cls,
+            application_instance,
+            &run_it->second,
+            {},
+            {},
+            nullptr
+        };
+
+        interpreter->call(run_it_call);
+    }
+};
+
+std::shared_ptr<andy::lang::structure> create_app_class(andy::lang::interpreter* interpreter)
+{
+    auto AppClass = std::make_shared<andy::lang::structure>("Application");
+
+    AppClass->instance_methods = {
+        { "new", andy::lang::method("new", andy::lang::method_storage_type::instance_method, {}, [interpreter](std::shared_ptr<andy::lang::object> object, std::vector<std::shared_ptr<andy::lang::object>> params){
+            uvalang_ui_app* app = new uvalang_ui_app(interpreter, object->derived_instance);
+            object->set_native_ptr(app);
+
+            app->run(0, nullptr);
+
+            return nullptr;
+        })},
+    };
+
+    return AppClass;
+}
