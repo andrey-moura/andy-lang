d769a48077f49b2f08b0082714544114253ac96c
Andrey Moura <andreymds88@gmail.com>
2025-05-10 06:36:48 -0300

    wup: Changes

---
+++ src/builtin_libs.cpp
@@ -0,0 +1,13 @@
+#include <memory>
+
+#include "andy/lang/extension.hpp"
+
+extern std::shared_ptr<andy::lang::extension> create_tests_extension();
+extern std::shared_ptr<andy::lang::extension> create_ui_extension();
+extern std::shared_ptr<andy::lang::extension> create_ui_extension();
+
+void create_builtin_libs() {
+    andy::lang::extension::add_builtin(create_tests_extension());
+    andy::lang::extension::add_builtin(create_ui_extension());
+    andy::lang::extension::add_builtin(create_ui_extension());
+}
